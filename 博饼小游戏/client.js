bc33795774518a110d787a9cf29192125f34dcef
URANUSWEN <117824579+URANUSWEN@users.noreply.github.com>
2022-11-11 21:36:13 +0800

    Add files via upload

---
+++ 博饼小游戏/client.js
@@ -0,0 +1,25 @@
+var socket = io();
+var result;
+var no = 0;
+var btn = document.getElementById('pause');
+var text = document.getElementById('text');
+alert("haha3");
+//给submit按钮添加点击事件
+btn.onclick = function() {
+    alert("haha");
+    result = "haha ";
+    socket.emit('chat message', result); //发送消息到服务器
+    alert("haha1 ");
+}
+ 
+socket.on('chat message', function(msg) {
+    alert("本次结果为 " + msg + ",可以开始下一场博饼 ");
+});
+
+function myFun() {
+    socket.emit('people message', no);
+    no++;
+}
+socket.on('people message', function(msg) {
+    text.innerHTML = "welcome player " + msg;
+})
---
+++ 博饼小游戏/server.js
@@ -0,0 +1,31 @@
+const ws = require('nodejs-websocket')
+let count = 0;
+
+const server = ws.createServer(conn => {
+    console.log('新的连接');
+    count++
+    conn.userName = `用户${count}`
+    broadcast(`${conn.userName}进入了游戏`)
+    conn.on('text', data => {
+        broadcast(data)
+    })
+    conn.on('close', data => {
+            console.log('关闭连接')
+            count--
+            broadcast(`${conn.userName}离开了游戏`)
+        })
+        //发生异常触发
+    conn.on('error', data => {
+
+    })
+})
+
+function broadcast(msg) {
+    server.connections.forEach(item => {
+        item.send(msg)
+    })
+}
+ 
+server.listen(5500, () => {
+    console.log("正在监听当前端口")
+})
